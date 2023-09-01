02ef5736a3b0318c21d1df899669ba99b29eda0e
Si-creations <s.mrocek@gmail.com>
2023-09-01 20:33:10 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+// Adding styles
+import Style from "./styles/app.scss";
+// Import Util
+import data from "./data";
+
+function App() {
+  // Sate
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[2]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+    </div>
+  );
+}
+
+export default App;
